d80fcb4787649bc17e55d73ae8b1c406139897db
Kalininskiy Andrei <kresthaus@mail.ru>
2022-06-13 15:23:00 +0400

    Задание №3

--- Home №3/Program.cs
+++ Home №3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+  int i;
+ 
+            Console.Write("Введите число : ");
+ 
+            i = int.Parse(Console.ReadLine());
+ 
+            if (i % 2 == 0)
+ 
+            {
+ 
+                Console.Write("Введенное число является четным");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("Введенное число нечетное");
+ 
+                Console.Read();
+ 
+            }
