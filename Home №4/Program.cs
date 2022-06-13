1853e435609625790e812ee645d2960aeb71df1a
Kalininskiy Andrei <kresthaus@mail.ru>
2022-06-13 15:31:53 +0400

    Задание №4

--- Home №4/Program.cs
+++ Home №4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+    Console.Write("Начальное число: ");
+
+        int n = int.Parse(Console.ReadLine());
+
+    Console.Write("Конечное число: ");
+
+        int m = int.Parse(Console.ReadLine());
+
+        for (int i = n; i <= m; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
